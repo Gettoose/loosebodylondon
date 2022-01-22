1d71ef3bc46690a7ba42c84621c070f6009f5c0d
Gettoose <98094595+Gettoose@users.noreply.github.com>
2022-01-22 16:31:19 +0100

    Update nav.js

--- script/nav.js
+++ script/nav.js
@@ -1,10 +1,6 @@
-const MenuBtn = document.querySelector("#menu-btn1");
+const MenuBtn = document.querySelector("#menu-btn1", "#menu-btn2");
 
 MenuBtn.addEventListener("click", () => {
 	document.body.classList.toggle("show-menu");
-	
-const MenuBtn = document.querySelector("#menu-btn2");
 
-MenuBtn.addEventListener("click", () => {
-	document.body.classList.toggle("show-menu");
 });
